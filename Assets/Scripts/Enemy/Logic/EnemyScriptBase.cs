258ad3e65bfb1c79fd5bbe4adfe94c5ecd6da77a
Snafuedasusual <omarsnafu04@gmail.com>
2024-06-26 00:58:28 +0700

    Seventh
    added enemy states.

--- Assets/Scripts/Enemy/Logic/EnemyScriptBase.cs
+++ Assets/Scripts/Enemy/Logic/EnemyScriptBase.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 public class EnemyScriptBase : MonoBehaviour
 {
@@ -10,11 +12,35 @@ public class EnemyScriptBase : MonoBehaviour
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Transform targetPos;
     [SerializeField] float stopDistance;
+    [SerializeField] Vector3[] patrolRoute;
+    [SerializeField] float defaultSpeed;
+    [SerializeField] float defaultAcc;
+    [SerializeField] float chaseSpeed;
+    [SerializeField] float chaseAcc;
+
+    public enum EnemyState
+    {
+        Idle,
+        Patroling,
+        Searching,
+        Chasing,
+        ChasingLastKnown,
+        LookAroundChase,
+        LookAroundIdle
+    }
+
+    [SerializeField]public EnemyState state;
+    [SerializeField]private EnemyState defaultState;
+
+    public Transform targetPlayer;
+    public Transform seenPlayer;
 
     public void InflictDamage(Transform sender, float knockBackPwr, float damage)
     {
         if(HitCooldown == null)
         {
+            agent.isStopped = true;
+            agent.velocity = Vector3.zero;
             m_Material.color = Color.green;
             KnockBack(sender, knockBackPwr);
             StartCoroutine(Cooldown());
@@ -27,7 +53,8 @@ public class EnemyScriptBase : MonoBehaviour
     private IEnumerator Cooldown()
     {
         HitCooldown = Cooldown();
-        yield return new WaitForSecondsRealtime(1.5f);
+        yield return new WaitForSecondsRealtime(1f);
+        agent.isStopped = false;
         m_Material.color = Color.red;
         HitCooldown = null;
     }
@@ -38,26 +65,138 @@ public class EnemyScriptBase : MonoBehaviour
         rb.AddForce(direction * knockBackPwr, ForceMode.Impulse);
     }
 
-
-    private void Start()
+    private void StateController()
     {
-         m_Material.color = Color.red;
+        if (state == EnemyState.Chasing)
+        {
+            ChasePlayer(seenPlayer);
+        }
+        if (state == EnemyState.LookAroundIdle)
+        {
+            targetPlayer = null;
+            LookAroundIdle();
+        }
+        if (state == EnemyState.Patroling)
+        {
+            Patrolling();
+        }
+        if(state == EnemyState.LookAroundChase)
+        {
+            LookAroundChase();
+        }
     }
 
     public void ChasePlayer(Transform plrPos)
     {
-        agent.destination = plrPos.position;
-        agent.speed = 3.5f;
-        agent.acceleration = 50f;
-        float distance = Vector3.Distance(plrPos.position, transform.position);
-        if(distance < stopDistance)
-        {
-            agent.isStopped = true;
-        }
-        else
+        if(plrPos != null)
         {
             agent.isStopped = false;
             agent.destination = plrPos.position;
+            agent.speed = chaseSpeed;
+            agent.acceleration = chaseAcc;
+            transform.LookAt(plrPos);
+            float distance = Vector3.Distance(plrPos.position, transform.position);
+            if (distance < stopDistance)
+            {
+                agent.isStopped = true;
+                agent.velocity = Vector3.zero;
+            }
+            else
+            {
+            agent.isStopped = false;
+            agent.destination = plrPos.position;
+            }
         }
     }
+
+    float delayLook = 0;
+    private void LookAroundIdle()
+    {
+        agent.isStopped = true;
+        float lookRate = 3.5f;
+        if (lookRate < delayLook)
+        {
+            Vector3 direction = new Vector3(transform.position.x + Random.Range(-1f, 1f), transform.position.y, transform.position.z + Random.Range(-1f, 1f));
+            Quaternion lookDir = Quaternion.LookRotation(transform.position - direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookDir, 1);
+            delayLook = 0;
+        }
+        else
+        {
+            delayLook += Time.deltaTime;
+        }
+    }
+
+    float delayLookChase = 0f;
+    int looking = 0;
+    private void LookAroundChase()
+    {
+        int lookAmounts = 3;
+        float lookRate = 2f;
+        if (lookRate < delayLookChase && looking < lookAmounts)
+        {
+            Vector3 direction = new Vector3(transform.position.x + Random.Range(-1f, 1f), transform.position.y, transform.position.z + Random.Range(-1f, 1f));
+            Quaternion lookDir = Quaternion.LookRotation(transform.position - direction);
+            transform.rotation = Quaternion.Slerp(transform.rotation, lookDir, 1);
+            delayLookChase = 0;
+            looking++;
+        }
+        if (lookRate < delayLookChase && looking > lookAmounts)
+        {
+            state = EnemyState.Patroling;
+            looking = 0;
+            delayLookChase = 0;
+        }
+        if(lookRate > delayLookChase && looking < lookAmounts)
+        {
+            delayLookChase += Time.deltaTime;
+        }
+        else
+        {
+            state = EnemyState.Patroling;
+            looking = 0;
+            delayLookChase = 0;
+        }
+    }
+
+
+    private void Idle()
+    {
+        //Do Nothing
+    }
+
+    int indexPatrol = 0;
+    private void Patrolling()
+    {
+        agent.speed = defaultSpeed;
+        agent.acceleration = defaultAcc;
+        agent.isStopped = false;
+        agent.destination = patrolRoute[indexPatrol];
+        float distance = Vector3.Distance(transform.position, agent.destination);
+        if (distance <= 1)
+        {
+            indexPatrol++;
+        }
+        if (indexPatrol == patrolRoute.Length)
+        {
+            indexPatrol = 0;
+        }
+    }
+
+    private void Update()
+    {
+        StateController();
+    }
+
+    private void Start()
+    {
+        m_Material.color = Color.red;
+        state = defaultState;
+    }
+
+
+
+
+
+
 }
--- Assets/Scripts/Enemy/Senses/EnemySenses.cs
+++ Assets/Scripts/Enemy/Senses/EnemySenses.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemySenses : MonoBehaviour
@@ -12,29 +13,39 @@ public class EnemySenses : MonoBehaviour
     RaycastHit hitInfo;
     RaycastHit secondCheck;
 
-
+    private bool knowsTarget = false;
 
     private void EnemySight()
     {
-        bool seePlayer = RotaryHeart.Lib.PhysicsExtension.Physics.BoxCast(transform.position + Vector3.up * 1f, new Vector3(10f, 0.5f, 0.05f), transform.forward, out hitInfo, Quaternion.LookRotation(transform.forward), maxRayDist, player, RotaryHeart.Lib.PhysicsExtension.PreviewCondition.Both);
+        bool seePlayer = RotaryHeart.Lib.PhysicsExtension.Physics.BoxCast(transform.position + Vector3.up * 1f, new Vector3(10f, 0.5f, 0.05f), transform.forward, out hitInfo, Quaternion.LookRotation(transform.forward), maxRayDist, player);
         if (seePlayer)
         {
             Vector3 direction = hitInfo.transform.position - transform.position;
             float dotDir = Vector3.Dot(transform.forward, direction.normalized);
             if (dotDir >= minDotProduct)
             {
-                if (RotaryHeart.Lib.PhysicsExtension.Physics.Raycast(transform.position, direction + Vector3.up * 0.01f, out secondCheck, RotaryHeart.Lib.PhysicsExtension.PreviewCondition.Both))
+                if (RotaryHeart.Lib.PhysicsExtension.Physics.Raycast(transform.position, direction + Vector3.up * 0.01f, out secondCheck))
                 {
                     float distance = Vector3.Distance(secondCheck.transform.position, transform.position);
                     if(secondCheck.transform == hitInfo.transform && distance <= maxVision)
                     {
                         //Debug.Log("Player Seen!");
-                        enemyScript.ChasePlayer(secondCheck.transform);
-                        transform.LookAt(secondCheck.transform);
+                        enemyScript.state = EnemyScriptBase.EnemyState.Chasing;
+                        enemyScript.seenPlayer = secondCheck.transform;
+                        knowsTarget = true;
                     }
                     else
                     {
-                        Debug.Log("Player Lost!");
+                        if(knowsTarget == true)
+                        {
+                            enemyScript.state = EnemyScriptBase.EnemyState.LookAroundChase;
+                            enemyScript.seenPlayer = null;
+                            Debug.Log("Player Lost!");
+                        }
+                        else
+                        {
+                            enemyScript.seenPlayer = null;
+                        }
                     }
                 }
             }
